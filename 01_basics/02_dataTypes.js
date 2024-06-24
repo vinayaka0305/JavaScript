5988810488eb9a85fb190fed35336800cae6232c
vinayaka0305 <139207810+vinayaka0305@users.noreply.github.com>
2024-06-24 11:32:52 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3)  we are use node js not browser
+
+let name = "vinayaka"
+let age = 28
+let isLoggedIn = false;
+
+
+//premitive
+// number -> 2 to power of 53 this is a rangle 
+// bigInt
+// string => ""
+// boolean => false or true
+// null  => // it is stand alone value (it is also a type) and 
+            //it is represantation of a empty value.
+
+//undefined => means the value is not defined
+//symbol => unique 
+
+//non premitive
+//object 
+
+console.log(typeof null);// object
+console.log(typeof undefined)// undefined (because it is a type of js)
+
