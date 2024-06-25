6074ed915d6b7dc55c9b38e396c53f93644ab2a8
vinayaka0305 <139207810+vinayaka0305@users.noreply.github.com>
2024-06-25 07:51:03 +0000

    comparator

---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,13 @@
+// console.log(null > 0); //false
+// console.log(null == 0);//false
+// console.log(null >= 0);// true
+
+
+console.log(undefined > 0); //false
+console.log(undefined == 0);// false
+console.log(undefined >= 0); // false
+
+// strict check 
+ 
+console.log("1" === 1); //false (it will check data types also)
+
