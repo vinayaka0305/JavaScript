fe6741c65bdfdc91b26368f49144dca7b6e626b9
vinayaka0305 <139207810+vinayaka0305@users.noreply.github.com>
2024-06-25 06:49:49 +0000

    conversions

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,49 @@
+// let score = "45abc";
+// // console.log(typeof score);
+// // console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof (valueInNumber));// o/p is number
+// console.log(valueInNumber); // o/p is NaN
+
+// let score = null;
+// let valueInNumber = Number(null)
+// console.log(valueInNumber);//it will null val  into zero
+// console.log( typeof (score)); // o/p is object
+
+// let score = undefined;
+// let valueInNumber = Number(score);
+// console.log(typeof(score));// o/p is undefined
+// console.log(valueInNumber); //o/p is NaN
+// console.log(typeof (valueInNumber)); // o/p is number
+
+// let score = true;
+// let valueInNumber = Number(true);
+// console.log(typeof (score))//boolean
+// console.log(valueInNumber); // o/p is 1
+// console.log(typeof (valueInNumber)); // number
+
+// let score = "vinayaka"
+
+// console.log(typeof (score));// o/p is string
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));//o/p is number
+// console.log(valueInNumber); // o/p is NaN
+
+// let isLoggedIn = "vinayaka";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// // console.log(booleanIsLoggedIn); //1 => true; 0 => false 
+// console.log(booleanIsLoggedIn); // for empty string "" => false , "vinayaka" => true;
+
+let num = 33;
+
+let stringNumber = String(num);
+console.log(stringNumber);// o/p is 33
+console.log(typeof (stringNumber)) //o/p is string
+
+
+
+
+
+
