afb39a6ed8c8db980ba4a76be2cda710b66696ab
vinayaka0305 <139207810+vinayaka0305@users.noreply.github.com>
2024-06-25 07:27:21 +0000

    operators

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,46 @@
+let val = 3;
+let negValue = -val;
+// console.log(negValue); // -3
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 3); // remainder
+// console.log( 2 ** 3) // 2 to the power of 3
+
+let st1 = "Hello"
+let st2 = " vinayaka"
+let st3 = st1 + st2;
+
+// console.log(st3);
+
+// console.log(2 + 2); // 4
+// console.log("1" + 2);//12 
+// console.log(1 + "2");//12
+// console.log(1 + 2 + "2"); // 32 
+// console.log("1" + 2 + 2);// 122 
+
+// let x = 3;
+// let y = x++; // firts it will asingn the value and then it will increment(post increment)
+
+// console.log(x , y); // (4, 3)
+
+// let a = 3;
+// let b = ++a; // here first it will increment and it will asign(pre increment)
+
+// console.log(a , b); //(4, 4)
+
+let x = 3;
+let y = x--;
+
+console.log(x, y) // (2, 3)
+
+let a = 3;
+let b = --a;
+
+console.log(a, b) // (2, 2)
+
+
+
+
