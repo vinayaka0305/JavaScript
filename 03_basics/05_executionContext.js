605ac232680a8f31be2a52ce2bc49ac917a9a937
vinayaka0305 <139207810+vinayaka0305@users.noreply.github.com>
2024-06-28 15:32:26 +0000

    execution context

---
+++ 03_basics/05_executionContext.js
@@ -0,0 +1,43 @@
+// JavaScript Execution Context
+// When JavaScript code runs, it does so in a specific context that determines
+// how variables and functions are accessed and executed.
+
+
+// There are three types of execution contexts:
+
+// 1.Global Execution Context: This is the default context where the code starts executing. 
+// It includes everything that’s not inside a function.
+
+// 2.Function Execution Context: Each time a function is called,
+// a new execution context is created for that function.
+
+// 3.Eval Execution Context: This is created when code is executed inside an eval function.
+// Phases of Execution
+
+
+// JavaScript executes in two main phases:
+
+// 1.Memory Creation Phase (Hoisting):
+
+// * Before any code runs, 
+// JavaScript scans through the code and allocates memory for variables and functions.
+//  * Variables are set to undefined, 
+//  and function declarations are stored in memory with their actual definitions.
+
+
+//2.Execution Phase:
+
+// In this phase, the code is executed line by line.
+// Variables get assigned their values.
+// Functions are called and executed.
+
+
+// Summary
+
+// Execution Contexts: JavaScript runs in different contexts (global, function, eval).
+
+// Two Phases:
+
+// Memory Creation: Sets up the variables and functions.
+
+// Execution: Runs the code and performs operations.
