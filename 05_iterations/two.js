606337475e2f105ddea0ba8d8459ee5db39f3c07
vinayaka0305 <139207810+vinayaka0305@users.noreply.github.com>
2024-06-29 01:09:58 +0000

    while and do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,33 @@
+// while loop
+
+// let i = 1;
+// while (i <= 10) {
+//     console.log(`the value of indexs are ${i}`)
+//     i++;
+// }
+
+
+let arr = ["ca", "im", "t"]
+
+let i = 0;
+while (i < arr.length) {
+    console.log(arr[i]);
+    i++;
+}
+
+// do while loop
+
+//example 1
+
+// let score = 1
+// do {
+//     console.log(`score is ${score}`);
+//     score++;
+// } while (score<=10);
+
+//example 2
+let score = 11
+do {
+    console.log(`score is ${score}`); // here it prints score 11 because of the syntax and it will check
+    score++; // - condition after that
+} while (score <= 10);
